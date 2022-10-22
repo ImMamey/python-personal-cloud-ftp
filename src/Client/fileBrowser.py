2f4484f0f0249e8b3847f70bfc405aae4e8773b0
ImMamey <32584037+ImMamey@users.noreply.github.com>
2022-10-22 00:26:39 -0400

    [feat] Functional File browser
    A fully functional file browser

--- src/Client/fileBrowser.py
+++ src/Client/fileBrowser.py
@@ -1,5 +1,5 @@
 from PyQt5 import QtWidgets, QtGui, QtCore
-
+import os
 from ui import main
 
 
@@ -7,15 +7,29 @@ class MyFileBrowser(main.Ui_MainWindow, QtWidgets.QMainWindow):
     def __init__(self):
         super(MyFileBrowser, self).__init__()
         self.setupUi(self)
+        self.treeView.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
+        self.treeView.customContextMenuRequested.connect(self.context_menu)
         self.populate()
 
     def populate(self):
         path = "C:\Windows"
-        model = QtWidgets.QFileSystemModel()
-        model.setRootPath((QtCore.QDir.rootPath()))
-        self.treeView.setModel(model)
-        self.treeView.setRootIndex(model.index(path))
+        self.model = QtWidgets.QFileSystemModel()
+        self.model.setRootPath((QtCore.QDir.rootPath()))
+        self.treeView.setModel(self.model)
+        self.treeView.setRootIndex(self.model.index(path))
         self.treeView.setSortingEnabled(True)
+
+    def context_menu(self):
+        menu = QtWidgets.QMenu()
+        open = menu.addAction("Open")
+        open.triggered.connect(self.open_file)
+        cursor = QtGui.QCursor()
+        menu.exec_(cursor.pos())
+    def open_file(self):
+        index = self.treeView.currentIndex()
+        file_path = self.model.filePath(index)
+        os.startfile(file_path)
+
 
 
 if __name__ == "__main__":
