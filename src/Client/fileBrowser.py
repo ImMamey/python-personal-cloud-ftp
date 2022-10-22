e3c439132d65b5ffc1643ee97266e33c76fd8632
ImMamey <32584037+ImMamey@users.noreply.github.com>
2022-10-21 22:20:05 -0400

    [feat] Basic File browser

---
+++ src/Client/fileBrowser.py
@@ -0,0 +1,24 @@
+from PyQt5 import QtWidgets, QtGui, QtCore
+
+from ui import main
+
+
+class MyFileBrowser(main.Ui_MainWindow, QtWidgets.QMainWindow):
+    def __init__(self):
+        super(MyFileBrowser, self).__init__()
+        self.setupUi(self)
+        self.populate()
+
+    def populate(self):
+        path = "C:\Windows"
+        model = QtWidgets.QFileSystemModel()
+        model.setRootPath((QtCore.QDir.rootPath()))
+        self.treeView.setModel(model)
+        self.treeView.setRootIndex(model.index(path))
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+    fb = MyFileBrowser()
+    fb.show()
+    app.exec_()
