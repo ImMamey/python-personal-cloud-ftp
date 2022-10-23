9d5ba8bb34d76ab0d143ebc74356cfc594747101
ImMamey <32584037+ImMamey@users.noreply.github.com>
2022-10-23 19:34:51 -0400

    .
    .

---
+++ src/Client/login.py
@@ -0,0 +1,26 @@
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+
+class LoginDialog(QDialog):
+    def __init__(self, parent=None, username=None):
+        super(LoginDialog, self).__init__(parent)
+
+        self.nameEdit = QLineEdit()
+        self.nameEdit.setText(username)
+        self.passEdit = QLineEdit()
+        self.passEdit.setEchoMode(QLineEdit.Password)
+        self.submitButton = QPushButton(QCoreApplication.translate("LoginDialog", "&Submit"))
+        self.submitButton.clicked.connect(self.accept)
+
+
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.nameEdit)
+        layout.addWidget(self.passEdit)
+        layout.addWidget(self.submitButton)
+
+        self.setLayout(layout)
+        self.setWindowTitle(QCoreApplication.translate("LoginDialog", "Login"))
+
+        if self.nameEdit.text() != None and len(self.nameEdit.text()) != 0:
+            self.passEdit.setFocus()
---
+++ src/Client/qftp.py
@@ -0,0 +1,48 @@
+from ftplib import FTP
+from urllib.parse import urlparse
+
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+from form_ui import Ui_MainWindow
+from src.Client.utils import *
+from login import LoginDialog
+
+
+class FtpWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        self.btnOpen.clicked.connect(self.connectFTP)
+        self.txtFtpUrl.returnPressed.connect(self.btnOpen.click)
+
+    def connectFTP(self):
+        host = self.txtFtpUrl.text()
+        user = None
+        passwd = None
+        port = 21
+        if url_check(self.txtFtpUrl.text()):
+            res = urlparse(self.txtFtpUrl.text())
+            host = res.hostname
+            user = res.username
+            passwd = res.password
+            port = res.port if res.port else 21
+
+        if user is not None and passwd is None:
+            loginDialog = LoginDialog(self, user)
+            loginDialog.exec()
+            user = loginDialog.nameEdit.text()
+            passwd = loginDialog.passEdit.text()
+
+        self.lstFileList.clear()
+        self.lstFileList.addItem("Loading...")  # FIXME: Doesn't work
+
+        ftp = FTP()
+        ftp.connect(host, port)
+        ftp.login(user, passwd)
+        flist = []
+        ftp.retrlines('LIST', flist.append)
+        flist = parse_ftp(flist)
+        self.lstFileList.clear()
+        for f in flist:
+            self.lstFileList.addItem(f.filename)
