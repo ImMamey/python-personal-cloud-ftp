75f69ff19853c10ece827bc1e9cacb775bc4b27b
ImMamey <32584037+ImMamey@users.noreply.github.com>
2023-01-17 21:33:45 -0400

    Update app.py

--- src/Server/app.py
+++ src/Server/app.py
@@ -10,11 +10,16 @@ from pyftpdlib.servers import ThreadedFTPServer
 import sys
 import os
 import threading
+import os
+import logging
+import platform
+import sqlite3
 from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
 from src.Server.utils import setup_logger, user_dir, get_ip
 from environment import env
+from src.Server.adminpanel import Ui_MainWindow
 
 LOG = logging.getLogger("server")
 
@@ -23,8 +28,10 @@ class Example(QtWidgets.QMainWindow):
     def __init__(self):
         super(Example, self).__init__()
         uic.loadUi('gui.ui', self)
+        self.username = ""
+        self.password = ""
 
-        #actions and events
+
         self.pushButtonStart.clicked.connect(self.onClick)
         self.pushButtonStop.clicked.connect(self.onStop)
         self.pushButtonCreate.clicked.connect(self.create_clicked)
@@ -40,30 +47,32 @@ class Example(QtWidgets.QMainWindow):
         # set a limit for connections
         self.max_cons = 256
         self.max_cons_per_ip = 5
+
     def lineEdite_validation(self) -> bool:
-        if self.lineEditPassword.text() == "" and self.lineEditUsername.text() == "":
+        if self.lineEditPassword.text() == "" and self.lineEditUser.text() == "":
             self.checkinputlabel.setText("Credentials cant be empty")
-            self.lineEditUsername.setText("")
+            self.lineEditUser.setText("")
             self.lineEditPassword.setText("")
             return False
 
         elif self.lineEditPassword.text() == "":
             self.checkinputlabel.setText("Please enter a valid password")
-            self.lineEditUsername.setText("")
+            self.lineEditUser.setText("")
             self.lineEditPassword.setText("")
             return False
 
-        elif self.lineEditUsername.text() == "":
+        elif self.lineEditUser.text() == "":
             self.checkinputlabel.setText("Please enter a valid username")
-            self.lineEditUsername.setText("")
+            self.lineEditUser.setText("")
             self.lineEditPassword.setText("")
             return False
-        elif self.lineEditPassword.text() != "" and self.lineEditUsername.text() != "":
+        elif self.lineEditPassword.text() != "" and self.lineEditUser.text() != "":
             return True
+
     def create_clicked(self) -> None:
 
         if self.lineEdite_validation():
-            username = self.lineEditUsername.text()
+            username = self.lineEditUser.text()
             password = self.lineEditPassword.text()
 
             msg = QMessageBox()
@@ -80,9 +89,91 @@ class Example(QtWidgets.QMainWindow):
             msg.buttonClicked.connect(self.ok_button_clicked)
             x = msg.exec_()
 
+    def sql_admin_login(self) -> None:
+        """
+        Logs in with an existing admin account if possible
+        :return:None
+        """
+        # Saves current data from the Usernames
+        if self.lineEdite_validation():
+            self.username = self.lineEditUser.text()
+            self.password = self.lineEditPassword.text()
+            db = None
+            try:
+                db = sqlite3.connect("users.db")
+            except Exception as e:
+                print(e)
+
+            # SQL commands and cursors
+            command_login = f'''SELECT username FROM admins where username = "{self.username}" and password = "{self.password}";'''
+            cursor = db.cursor()
+
+            # Check for admin match
+            cursor.execute(command_login)
+            if not cursor.fetchone():
+                msg = QMessageBox()
+                msg.setWindowTitle("Failiure!")
+                msg.setText(f"Admin credentials are wrong, please try again")
+                msg.setStandardButtons(QMessageBox.Retry)
+                x = msg.exec_()
+            else:
+                print("Logged in!")
+                self.openWindow()
+
+            db.commit()
+            db.close()
+
+    def sql_create_admin(self) -> None:
+        """
+        Create a db connection, and creates and admin if possible
+        :return: None
+        """
+        # connection with DB
+        db = None
+        try:
+            db = sqlite3.connect("users.db")
+        except Exception as e:
+            print(e)
+
+        # SQL commands and cursors
+        command_check = f'SELECT COUNT(*) FROM admins'
+        command_insert = f'INSERT INTO admins(username, password) values(? , ?)'
+        command_chkrepeatedadmin = f'SELECT username FROM admins where username = ?;'
+        cursor = db.cursor()
+
+        # CHECKS IF there are more than 2 ADMINS
+        cursor.execute(command_check)
+        value = cursor.fetchone()
+
+        if value[0] == 2:
+            msg = QMessageBox()
+            msg.setWindowTitle("You cant create more admins")
+            msg.setText(f"You only can have 2 admins accounts with this program.")
+            msg.setStandardButtons(QMessageBox.Ok)
+            x = msg.exec_()
+
+        elif value[0] < 2:
+            cursor.execute(command_chkrepeatedadmin, (self.username,))
+            if not cursor.fetchone():
+                print("Repeated admin")
+            else:
+                print("Created 1 admin")
+                cursor.execute(command_insert, (self.username, self.password))
+                # src.Server.ftpserver.start()
+        db.commit()
+        db.close()
+
+
+    def ok_button_clicked(self, i):
+        if i.text() == "OK":
+            self.sql_create_admin()
+        if i.text() == "Retry":
+            self.username = ""
+            self.password = ""
+
     def onClick(self):
         print('start')
-
+        server_ip = get_ip()
         user = self.lineEditUser.text()
         passw = self.lineEditPassword.text()
 
@@ -92,6 +183,14 @@ class Example(QtWidgets.QMainWindow):
 
         QMessageBox.information(self, "FTP Server started", "FTP Server started")
         self.start()
+
+    def openWindow(self):
+        self.window = QtWidgets.QMainWindow()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self.window)
+        self.window.show()
+        self.ui.refreshed_button_pressed()
+        win.close()
 
     def _run_server(self):
         self.server.serve_forever()
